373522d9d85bf501321ffa1243a8d14651558a9b
SaranshSinha <31996102+SaranshSinha@users.noreply.github.com>
2019-10-09 12:04:09 +0530

    Update 6b.cpp

--- 6b.cpp
+++ 6b.cpp
@@ -1,34 +1,54 @@
-#include<stdio.h>
-#include<conio.h>
-const int n=4;
-int indegree[n];
-int a[n][n]={0};
-void topo_order()
-{
-	for(int i=0;i<n;i++)
-	{
-		indegree[i]=0;
-		for(int j=0;j<n;j++)
-			indegree[i] += a[j][i];
-	}
-	for(int j=0;j<n;j++)
-		if(indegree[j]==0)
-		{
-			indegree[j]=-1;
-			printf("%d",j+1);
-			for(int k=0;k<n;k++)
-			{
-				if(a[j][k]==1)
-				indegree[k]=indegree[k]-1;
-			}
-		}
+#include<iostream>
+using namespace std;
+
+int tasks[10][10],n;
+
+void input(){
+    int prev_task=0, task=0;
+    cout<<"enter no. of tasks \n";
+    cin>>n;
+    for(int i =0;i<n;i++){
+        for(int j=0;j<n;j++){
+            tasks[i][j]=0;
+        }
+    }
+    while(task != -1 || prev_task !=-1){
+        cout<<"enter dependencies [task dependency]........[-1 -1] to stop\n";
+        cin>>task>>prev_task;
+        if(task!= -1 || prev_task!=-1)
+            tasks[prev_task][task]=1;
+        }
 }
-int main()
-{
-	printf("\nEnter the adjacency matrix : \n");
-	for(int i=0;i<n;i++)
-		for(int j=0;j<n;j++)
-			scanf("%d",&a[i][j]);
-	topo_order();
-	return 0;
+
+void topologicalOrder(){
+    int indegree[10],k,stack[10],top=-1;
+    for(int i=0;i<n;i++){
+        indegree[i]=0;
+        for(int j=0;j<n;j++){
+            indegree[i] +=tasks[j][i];
+        }
+    }
+    for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                stack[++top]=i;
+            }
+    }
+    while(top!=-1 ){
+            k=stack[top--];
+            cout<<k<<"\t";
+            indegree[k]=-1;
+            for(int l=0;l<n;l++){
+                if(tasks[k][l]==1){
+                    indegree[l] -=1;
+                    if(indegree[l]== 0)
+                        stack[++top]=l;
+                }
+            }
+    }
+    cout<<"\n";
+}
+int main(){
+    input();
+    topologicalOrder();
+    return 0;
 }
